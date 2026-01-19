859d53bf0eb8a5f2c6a7174c09b6b659b9fafe3b
prothegee <prothegee@proton.me>
2026-01-19 15:48:51 +0700

    use user environment instead

--- drun-grpc_server.sh
+++ drun-grpc_server.sh
@@ -3,10 +3,6 @@ set -e;
 
 export CURRENT_DIR="$(pwd)";
 
-export C_INCLUDE_PATH="$HOME/include";
-export LIBRARY_PATH="$HOME/lib";
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$LIBRARY_PATH"
-
 cd "$CURRENT_DIR/cmd/grpc_server";
 go run .;
 
